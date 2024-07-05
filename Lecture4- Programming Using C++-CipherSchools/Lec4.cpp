f11d89fdf43b5cbbc805f93bd3ee1d870a68f7cb
Ashish Shaik <118331953+Ashishshaik91@users.noreply.github.com>
2024-07-05 22:37:12 +0530

    Add files via upload

---
+++ Lecture4- Programming Using C++-CipherSchools/Lec4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int a=5;
+    int b=10;
+    int c=a+b;
+
+    cout<<"Sum of these two integers are "<<c<<"\n"; //uses a separate variable to store the value of the sum for further use.
+    cout<<a+b<<"\n"; //it is one of the way to print a direct output.
+    return 0;
+}
