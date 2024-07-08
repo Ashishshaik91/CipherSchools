ae479ed5d186d282c5e2bafa9df0d878fdac6664
Ashish Shaik <118331953+Ashishshaik91@users.noreply.github.com>
2024-07-08 14:57:43 +0530

    Add files via upload

---
+++ Lecture10- Programming Using C++-CipherSchools/lec10.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int i;
+    for(i=0;i<101;i++) //initalizing the for loop with conditions.
+    cout<<i<<" ";
+
+    //below is the post increment pre increment codes examples from the lecture.
+
+    /*int i=0;
+    int j=0;
+    int k=0;
+    cout<<"The values of I J K are "<<i<<" "<<j<<" "<<k<<"\n";
+    j=i++;
+    cout<<"The values of I J K are "<<i<<" "<<j<<" "<<k<<"\n";
+    k=++i;
+    cout<<"The values of I J K are "<<i<<" "<<j<<" "<<k<<"\n";*/
+
+    /*int i=1;
+    cout<<++i;
+    cout<<i;*/
+
+    return 0;
+}
